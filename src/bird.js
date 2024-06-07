46d1a5ef05e08875aa3b56949a95d6a527a20361
Farshad <f.bagdeli13@gmail.com>
2024-06-07 12:31:07 +0200

    bird class Done

---
+++ src/bird.js
@@ -0,0 +1,11 @@
+import { Animal } from "./animal.js";
+
+class Bird extends Animal {
+  constructor(name, age, lifespan) {
+    super(name, age, lifespan)
+  }
+
+  sound() {
+    return `${this.name} Say tweet!`
+  }
+}
