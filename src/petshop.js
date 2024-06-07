9ced322d0de69d8455308a57c83ba3782c22d84c
Farshad <f.bagdeli13@gmail.com>
2024-06-07 17:00:09 +0200

    All tests Done

--- src/petshop.js
+++ src/petshop.js
@@ -47,7 +47,7 @@ class PetShop {
       return pet.name === animal.name
     })
     if(found === undefined) {
-      return `The ${animal} dose not exist!`
+      return `The ${animal.name} dose not exist!`
     }
     this.petList = this.petList.filter((pet) => {
       return pet.name !== animal.name
@@ -74,24 +74,4 @@ class PetShop {
   }
 }
 
-const myDog = new Dog('Pilar', 6.5, 14)
-const myCat = new Cat('Garffield', 6.5, 6)
-const myBird = new Bird('tweet', 6.5, 3)
-const myPetShop = new PetShop()
-
-myPetShop.addDog(myDog)
-myPetShop.addBird(myBird)
-myPetShop.addCat(myCat)
-
-// console.log('dogs : ', myPetShop.dogList)
-// console.log('cats : ', myPetShop.catList)
-// console.log('birds : ', myPetShop.birdList)
-
-console.log(myPetShop.removeAnimal(myDog))
-console.log('all pets : ', myPetShop.petList)
-
-console.log(myPetShop.removeAnimal(myBird))
-console.log('all pets : ', myPetShop.petList)
-
-console.log(myPetShop.removeAnimal(myCat))
-console.log('all pets : ', myPetShop.petList)
+export { PetShop }
