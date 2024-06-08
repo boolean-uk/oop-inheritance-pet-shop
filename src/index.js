befc75965eb2d3fd1a4d770d03806af2b477f5ee
Leonardo Lodi <your_email@address>
2024-06-08 02:52:21 +0100

    remove animal types classes, add stocklimit to breed and animal type, test it and throw errors

--- src/index.js
+++ src/index.js
@@ -1,151 +1,143 @@
 class PetShop {
-    constructor() {
-        this.animalsList = []
+  constructor() {
+    this.animalsList = []
+    this.stockLimit = {
+      Labrador: 4,
+      GermanShepherd: 8,
+      Siamese: 5,
+      MaineCoon: 7,
+      Budgerigar: 9,
+      Parrot: 6,
+      Betta: 8,
+      NeonTetra: 10,
+    }
+    this.totalStockLimit = {
+      Dog: 10,
+      Cat: 12,
+      Bird: 14,
+      Fish: 16,
+    }
+  }
+
+  addAnimal(animal) {
+    const breedType = animal.constructor.name
+    const mainType =
+      breedType === 'Labrador' || breedType === 'GermanShepherd'
+        ? 'Dog'
+        : breedType === 'Siamese' || breedType === 'MaineCoon'
+        ? 'Cat'
+        : breedType === 'Budgerigar' || breedType === 'Parrot'
+        ? 'Bird'
+        : breedType === 'Betta' || breedType === 'NeonTetra'
+        ? 'Fish'
+        : null
+
+    if (mainType === null) {
+      throw new Error(`Invalid breed type: ${breedType}`)
     }
 
-    addAnimal(animal) {
-        this.animalsList.push(animal)
+    if (this.animalsList.some((a) => a.name === animal.name)) {
+      throw new Error(`An animal with name ${animal.name} already exists.`)
     }
+
+    if (
+      this.animalsList.filter((a) => a.constructor.name === breedType).length >=
+      this.stockLimit[breedType]
+    ) {
+      throw new Error(
+        `The ${breedType} stock limit of ${this.stockLimit[breedType]} has been reached.`
+      )
+    }
+
+    if (this.animalsList.length >= this.totalStockLimit[mainType]) {
+      throw new Error(`The ${mainType} stock limit of ${this.totalStockLimit[mainType]} has been reached.`)
+    }
+
+    this.animalsList.push(animal)
+  }
 }
 
 class Animal extends PetShop {
-    constructor(name, age, expectedLifeSpan, quantity) {
-        super()
-
-        if (this.isString(name) && this.isNumber(age, expectedLifeSpan, quantity)) {
-            this.name = name
-            this.age = age
-            this.expectedLifeSpan = expectedLifeSpan
-            this.quantity = quantity
-        } else {
-            throw new Error('Animal properties not provided correctly')
-        }
+  constructor(name, age, expectedLifeSpan) {
+    super()
+    if (this.isString(name) && this.isNumber(age, expectedLifeSpan)) {
+      this.name = name
+      this.age = age
+      this.expectedLifeSpan = expectedLifeSpan
+    } else {
+      throw new Error("The properties weren't filled in correctly.")
     }
+  }
 
-    isString(name) {
-        return typeof name === 'string' && name !== ''
-    }
+  isString(name) {
+    return typeof name === 'string' && name !== ''
+  }
 
-    isNumber(...props) {
-        return props.every(prop => typeof prop === 'number' && prop > 0)
-    }
+  isNumber(...props) {
+    return props.every((prop) => typeof prop === 'number' && prop > 0)
+  }
 
-    makeSound() {
-        return `${this.name} made a sound`
-    }
+  makeSound() {
+    return `${this.name} made a sound.`
+  }
 }
 
-class Dog extends Animal {
-    constructor(name, age, expectedLifeSpan, quantity) {
-        super(name, age, expectedLifeSpan, quantity)
-    }
+class Labrador extends Animal {
+  constructor(name, age, expectedLifeSpan) {
+    super(name, age, expectedLifeSpan)
+  }
 }
 
-class Cat extends Animal {
-    constructor(name, age, expectedLifeSpan, quantity) {
-        super(name, age, expectedLifeSpan, quantity)
-    }
+class GermanShepherd extends Animal {
+  constructor(name, age, expectedLifeSpan) {
+    super(name, age, expectedLifeSpan)
+  }
 }
 
-class Bird extends Animal {
-    constructor(name, age, expectedLifeSpan, quantity) {
-        super(name, age, expectedLifeSpan, quantity)
-    }
+class Siamese extends Animal {
+  constructor(name, age, expectedLifeSpan) {
+    super(name, age, expectedLifeSpan)
+  }
 }
 
-class Fish extends Animal {
-    constructor(name, age, expectedLifeSpan, quantity) {
-        super(name, age, expectedLifeSpan, quantity)
-    }
+class MaineCoon extends Animal {
+  constructor(name, age, expectedLifeSpan) {
+    super(name, age, expectedLifeSpan)
+  }
 }
 
-class Labrador extends Dog {
-    constructor(name, age, expectedLifeSpan, quantity) {
-        super(name, age, expectedLifeSpan, quantity)
-        this.stockLimit = 5
-        
-        if (quantity > this.stockLimit) {
-            throw new Error(`This animal has an stock of ${this.stockLimit}, can not choose more than its limit`)
-        }
-    }
+class Budgerigar extends Animal {
+  constructor(name, age, expectedLifeSpan) {
+    super(name, age, expectedLifeSpan)
+  }
 }
 
-class GermanShepherd extends Dog {
-    constructor(name, age, expectedLifeSpan, quantity) {
-        super(name, age, expectedLifeSpan, quantity)
-        this.stockLimit = 8
-        
-        if (quantity > this.stockLimit) {
-            throw new Error(`This animal has an stock of ${this.stockLimit}, can not choose more than its limit`)
-        }
-    }
+class Parrot extends Animal {
+  constructor(name, age, expectedLifeSpan) {
+    super(name, age, expectedLifeSpan)
+  }
 }
 
-class Siamese extends Cat {
-    constructor(name, age, expectedLifeSpan, quantity) {
-        super(name, age, expectedLifeSpan, quantity)
-        this.stockLimit = 7
-        
-        if (quantity > this.stockLimit) {
-            throw new Error(`This animal has an stock of ${this.stockLimit}, can not choose more than its limit`)
-        }
-    }
+class Betta extends Animal {
+  constructor(name, age, expectedLifeSpan) {
+    super(name, age, expectedLifeSpan)
+  }
 }
 
-class MaineCoon extends Cat {
-    constructor(name, age, expectedLifeSpan, quantity) {
-        super(name, age, expectedLifeSpan, quantity)
-        this.stockLimit = 4
-        
-        if (quantity > this.stockLimit) {
-            throw new Error(`This animal has an stock of ${this.stockLimit}, can not choose more than its limit`)
-        }
-    }
+class NeonTetra extends Animal {
+  constructor(name, age, expectedLifeSpan) {
+    super(name, age, expectedLifeSpan)
+  }
 }
 
-class Budgerigar extends Bird {
-    constructor(name, age, expectedLifeSpan, quantity) {
-        super(name, age, expectedLifeSpan, quantity)
-        this.stockLimit = 5
-        
-        if (quantity > this.stockLimit) {
-            throw new Error(`This animal has an stock of ${this.stockLimit}, can not choose more than its limit`)
-        }
-    }
+export {
+  Labrador,
+  GermanShepherd,
+  Siamese,
+  MaineCoon,
+  Budgerigar,
+  Parrot,
+  Betta,
+  NeonTetra,
 }
-
-class Parrot extends Bird {
-    constructor(name, age, expectedLifeSpan, quantity) {
-        super(name, age, expectedLifeSpan, quantity)
-        this.stockLimit = 2
-        
-        if (quantity > this.stockLimit) {
-            throw new Error(`This animal has an stock of ${this.stockLimit}, can not choose more than its limit`)
-        }
-    }
-}
-
-class Betta extends Fish {
-    constructor(name, age, expectedLifeSpan, quantity) {
-        super(name, age, expectedLifeSpan, quantity)
-        this.stockLimit = 6
-
-        if (quantity > this.stockLimit) {
-            throw new Error(`This animal has an stock of ${this.stockLimit}, can not choose more than its limit`)
-        }
-    }
-}
-
-class NeonTetra extends Fish {
-    constructor(name, age, expectedLifeSpan, quantity) {
-        super(name, age, expectedLifeSpan, quantity)
-        this.stockLimit = 3
-
-        if (quantity > this.stockLimit) {
-            throw new Error(`This animal has an stock of ${this.stockLimit}, can not choose more than its limit`)
-        }
-    }
-}
-
-export { Labrador, GermanShepherd, Siamese, MaineCoon, Budgerigar, Parrot, Betta, NeonTetra }
 export default PetShop
