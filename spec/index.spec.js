c024ffe641943a7bca91e97439a1fb11768af119
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-06-11 08:32:34 +0100

    Core: the pet shop is able to store various types of animals

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,4 +1,4 @@
-import PetShop from "../src/index.js";
+import PetShop, { AnimalType, Animal, Dog, Cat, Bird, Fish  } from "../src/index.js";
 
 describe("Core Criteria", () => {
     let petShop;
@@ -6,5 +6,21 @@ describe("Core Criteria", () => {
     it("should exist", () => {
       petShop = new PetShop([]);
       expect(petShop).toBeInstanceOf(PetShop);
-    })
+      
+      let animal = new Animal("cow", 4, 10, "moooooo");
+      expect(animal).toBeInstanceOf(Animal);
+      animal = new Dog("Charger", 3, 9, "woof");
+      expect(animal).toBeInstanceOf(Animal);
+      expect(animal).toBeInstanceOf(Dog);
+      animal = new Cat("whiskers", 3, 15, "meow");
+      expect(animal).toBeInstanceOf(Animal);
+      expect(animal).toBeInstanceOf(Cat);
+      animal = new Bird("Tweeny", 3, 15, "tweeeiiit");
+      expect(animal).toBeInstanceOf(Animal);
+      expect(animal).toBeInstanceOf(Bird);
+      animal = new Fish("Tinkerbell", 3, 15, "blooop blooop");
+      expect(animal).toBeInstanceOf(Animal);
+      expect(animal).toBeInstanceOf(Fish);
+    });
+    
 })
--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 export default class PetShop {
-
+construct(type) {
+    this.animalTypeStockLimit = {}
+    this.animals = []
+  }
 }
+
+export class AnimalType {
+    constructor(type, limit) {
+      (this.type = type), (this.limit = limit);
+    }
+  }
+  
+export class Animal {
+    name;
+    age;
+    xlifespan;
+    sound;
+  
+    constructor(name, age, xlifespan, sound) {
+        this.name = name
+        this.age = age
+        this.xlifespan = xlifespan
+        this.sound = sound
+    }
+}
+export class Dog extends Animal {
+    constructor(name, age, xlifespan, sound) {
+      super(name, age, xlifespan, sound);
+    }
+  }
+  export class Cat extends Animal {
+    constructor(name, age, xlifespan, sound) {
+      super(name, age, xlifespan, sound);
+    }
+  }
+  export class Bird extends Animal {
+    constructor(name, age, xlifespan, sound) {
+      super(name, age, xlifespan, sound);
+    }
+  }
+  export class Fish extends Animal {
+    constructor(name, age, xlifespan, sound) {
+      super(name, age, xlifespan, sound);
+    }
+  }
+  
+  
