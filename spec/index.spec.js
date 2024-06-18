456057a6424d4a82395bb81b5dcb33d7f8038566
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-06-18 18:51:22 +0100

    ww

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,7 +1,9 @@
 const Dog = require('../src/Dog')
 const Cat = require('../src/Cat')
 const Fish = require('../src/Fish')
+const Bird = require('../src/Bird')
 const PetShop = require('../src/PetShop')
+
 
 
 describe('PetShop', () => {
@@ -15,16 +17,19 @@ describe('PetShop', () => {
         const dog = new Dog('Rex', 2, 13)
         const cat = new Cat('PussyCat', 3, 17)
         const fish = new Fish('Fishfish', 9, 20)
+        const bird = new Bird('Chirps', 5, 99)
 
         petShop.addAnimal(dog)
         petShop.addAnimal(cat)
         petShop.addAnimal(fish)
+        petShop.addAnimal(bird)
 
         const animals = petShop.listAnimals()
         expect(animals.length).toBe(3) 
         expect(animals).toContain(dog)
         expect(animals).toContain(cat)
         expect(animals).toContain(fish)
+        expect(animals).toContain(bird)
     })
 })
 
@@ -33,9 +38,11 @@ describe('Animal Sounds', () => {
         const dog = new Dog('Rex', 2, 13)
         const cat = new Cat('PussyCat', 3, 17)
         const fish = new Fish('Fishfish', 9,20)
+        const bird = new Bird('Chirps', 5, 99)
 
         expect(dog.makesound()).toBe('woof!')
         expect(cat.makesound()).toBe('Meow Meow')
         expect(fish.makesound()).toBe('fish_sound')
+        expect(bird.makesound()).toBe('Chirp')
     })
 })
