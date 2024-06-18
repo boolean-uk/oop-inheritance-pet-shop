c474a819217f38447ebf04d7f237c9a0e5391700
Rafa Lopes <rafalopessecond@gmail.com>
2024-06-18 14:59:47 +0100

    Rafa Lopes

---
+++ spec/index.spec.js
@@ -0,0 +1,163 @@
+const PetShop = require("../src/index");
+
+class Animal {
+	constructor(name, lifespan, age) {
+		this.name = name;
+		this.lifespan = lifespan;
+		this.age = age;
+	}
+}
+class Dog extends Animal {
+	//does some stuff.
+}
+class Labrador extends Dog {
+	constructor(name, lifespan, age) {
+		super(name, lifespan, age);
+		this.breed = "Labrador";
+	}
+}
+class Poodle extends Dog {
+	constructor(name, lifespan, age) {
+		super(name, lifespan, age);
+		this.breed = "Poodle";
+	}
+}
+
+class Reptile extends Animal {}
+
+class Lizard extends Reptile {}
+class Snake extends Reptile {}
+class Iguana extends Reptile {}
+
+function defaultPriceFunction(pet, basePrice) {
+	return basePrice + pet.age * 0.5;
+}
+
+function labradorPriceFunction(pet) {
+	return defaultPriceFunction(pet, 100);
+}
+
+function poodlePriceFunction(pet) {
+	return defaultPriceFunction(pet, 150);
+}
+
+const dogShopSchema = [
+	{
+		class: Dog,
+		capacity: 2,
+		areas: [
+			{
+				class: Labrador,
+				capacity: 2,
+				priceFunction: labradorPriceFunction,
+			},
+			{
+				class: Poodle,
+				capacity: 1,
+				priceFunction: poodlePriceFunction,
+			},
+		],
+	},
+];
+
+const reptileShopSchema = [
+	{
+		class: Reptile,
+		capacity: 20,
+		areas: [
+			{
+				class: Lizard,
+				capacity: 5,
+				priceFunction: labradorPriceFunction,
+			},
+			{
+				class: Iguana,
+				capacity: 5,
+				priceFunction: poodlePriceFunction,
+			},
+			{
+				class: Snake,
+				capacity: 1,
+				priceFunction: labradorPriceFunction,
+			},
+		],
+	},
+];
+
+const zooShopSchema = [
+	{
+		class: Dog,
+		capacity: 2,
+		areas: [
+			{
+				class: Labrador,
+				capacity: 2,
+				priceFunction: labradorPriceFunction,
+			},
+			{
+				class: Poodle,
+				capacity: 1,
+				priceFunction: poodlePriceFunction,
+			},
+		],
+	},
+	{
+		class: Reptile,
+		capacity: 20,
+		areas: [
+			{
+				class: Lizard,
+				capacity: 5,
+				priceFunction: labradorPriceFunction,
+			},
+			{
+				class: Iguana,
+				capacity: 5,
+				priceFunction: poodlePriceFunction,
+			},
+			{
+				class: Snake,
+				capacity: 1,
+				priceFunction: labradorPriceFunction,
+			},
+		],
+	},
+];
+
+describe("petshop contructor", () => {
+	let dogPetShop, reptilePetShop;
+
+	beforeEach(() => {
+		dogPetShop = new PetShop(dogShopSchema);
+		reptilePetShop = new PetShop(reptileShopSchema);
+	});
+
+	it("Should initialize a shop with a schema and an empty animals array", () => {
+		expect(dogPetShop.totalAnimals).toBe(0);
+	});
+
+	it("Should only add an animal described in the pet shop schema", () => {
+		expect(dogPetShop.add(new Poodle("Missy", 10, 5))).toBe(true);
+		expect(dogPetShop.add(new Lizard("Lizardman", 10, 5))).toBe(false);
+	});
+
+	it("Should only remove an animal that is in the pet shop", () => {
+		dogPetShop.add(new Poodle("Missy", 10, 5));
+		console.log(dogPetShop.totalAnimals);
+
+		expect(dogPetShop.remove(new Poodle("Missy", 10, 5))).toBeDefined();
+		expect(
+			dogPetShop.remove(new Lizard("Lizardman", 10, 5))
+		).toBeUndefined();
+	});
+
+	it("Should not exceed the max capacity for the animal described in the pet shop schema", () => {
+        //pet shop only allows 2 dogs, either 2 labradors or 1 poodle and 1 labrador
+		expect(dogPetShop.add(new Poodle("Missy", 10, 5))).toBe(true);
+		expect(dogPetShop.add(new Poodle("eldooP", 10, 5))).toBe(false);
+		expect(dogPetShop.add(new Labrador("Max", 10, 5))).toBe(true);
+		expect(dogPetShop.add(new Labrador("Sirius", 10, 5))).toBe(false);
+		expect(dogPetShop.remove(new Poodle("Missy", 10, 5))).toBeDefined();
+		expect(dogPetShop.add(new Labrador("Sirius", 10, 5))).toBe(true);
+	});
+});
---
+++ src/index.js
@@ -0,0 +1,94 @@
+class PetShop {
+	#shop_schema;
+	#animals;
+
+	constructor(schema) {
+		this.#shop_schema = schema;
+
+		this.#animals = [];
+	}
+
+	add(animal) {
+		const animalGroup = this.#findAnimalGroupDataSchema(animal),
+			animalData = this.#findAnimalDataSchema(animal);
+
+		if (!animalData) return false;
+
+		if (animalGroup.capacity > 0 && animalData.capacity > 0) {
+			animalGroup.capacity--;
+			animalData.capacity--;
+			this.#animals.push(animal);
+			return true;
+		} else return false;
+	}
+
+	#findAnimalGroupDataSchema(animal) {
+		for (let animalGroup of this.#shop_schema) {
+			if (animal instanceof animalGroup.class) {
+				return animalGroup;
+			}
+		}
+
+		return undefined;
+	}
+
+	#findAnimalDataSchema(animal) {
+		const animalGroup = this.#findAnimalGroupDataSchema(animal);
+
+		if (!animalGroup) return undefined;
+
+		for (let data of animalGroup.areas) {
+			if (animal instanceof data.class) {
+				return data;
+			}
+		}
+	}
+
+	remove(animal) {
+		//get index
+		const index = this.#animals.findIndex(
+			(animalInStore) =>
+				animalInStore.name === animal.name &&
+				animalInStore.lifespan === animal.lifespan &&
+				animalInStore.age === animal.age
+		);
+		//
+		if (index === -1) return undefined;
+
+		//update capacity
+		const animalGroup = this.#findAnimalGroupDataSchema(animal),
+			animalData = this.#findAnimalDataSchema(animal);
+		animalData.capacity++;
+		animalGroup.capacity++;
+
+		//delete if exists
+		return this.#animals.splice(index, 1)[0];
+	}
+
+	purchase(animal) {
+		const shopAnimal = this.remove(animal);
+		const animalData = this.#findAnimalDataSchema(shopAnimal);
+
+		if (!shopAnimal) return undefined;
+
+		return {
+			animal: shopAnimal,
+			price: animalData.priceFunction(shopAnimal),
+		};
+	}
+
+	get totalAnimals() {
+		return this.#animals.length;
+	}
+
+	createAnimalGroupSection(animalGroupSchema) {
+		this.#shop_schema.push(animalGroupSchema);
+	}
+
+	createAnimalSection(animalGroupClass, animalSchema) {
+		//find index of animal group
+		//animalGroupSchema.areas.push(animalSchema)
+	}
+}
+
+module.exports = PetShop;
